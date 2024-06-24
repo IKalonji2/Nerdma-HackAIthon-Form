27ca84740e354632034acba26058b77fa545eb6b
Kgptso Phela <kgotso.phela@nerdma.co.za>
2024-06-24 09:26:13 +0200

    MainMenu visibility: hidden

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 import streamlit as st
+
 st.set_page_config(layout="wide")
+
 from forms import form1, form2, form3, form4, form5
 from utils import submit_application
 from header_html import header_html, centered_css
 
-
+hide_menu_style = """
+    <style>
+    #MainMenu {visibility: hidden;}
+    footer {visibility: hidden;}
+    </style>
+    """
+st.markdown(hide_menu_style, unsafe_allow_html=True)
 
 st.markdown(header_html, unsafe_allow_html=True)
 st.markdown(centered_css, unsafe_allow_html=True)
