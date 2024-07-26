e4e7e58e790c8bba04e3f7335ad547a74f975c47
KgotsoPhela <90217020+KgotsoPhela@users.noreply.github.com>
2024-07-26 12:04:56 +0200

    Update app.py
    Close applicatins

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ import streamlit as st
 
 st.set_page_config(layout="wide")
 
-from forms import form1, form2, form3, form4, form5
-from utils import submit_application
+# from forms import form1, form2, form3, form4, form5
+# from utils import submit_application
 from header_html import header_html, centered_css
 
 hide_menu_style = """
@@ -16,38 +16,30 @@ st.markdown(hide_menu_style, unsafe_allow_html=True)
 
 st.markdown(header_html, unsafe_allow_html=True)
 st.markdown(centered_css, unsafe_allow_html=True)
-st.write('<div class="content">', unsafe_allow_html=True)
 
-# Check if the form has been submitted
-if 'form_submitted' not in st.session_state:
-    st.session_state.form_submitted = False
+# Style for the card
+card_style = """
+    <style>
+    .card {
+        background: #f9f9f9;
+        border-radius: 10px;
+        padding: 20px;
+        margin: 50px auto;
+        max-width: 600px;
+        box-shadow: 0 4px 8px rgba(0,0,0,0.1);
+        text-align: center;
+    }
+    </style>
+    """
 
-# Create three columns
-col1, col2, col3 = st.columns([0.5, 2, 0.5])
+st.markdown(card_style, unsafe_allow_html=True)
 
-with col2:
-    if st.session_state.form_submitted:
-        st.markdown('''
-        <div class="centered-message">
-            <h2>Thank you for registering, we will be in contact soon with more details on the Hack-AI-thon</h2>
-        </div>
-        ''', unsafe_allow_html=True)
-    else:
-        st.text("")
-        st.text("")
-        st.text("")
-        st.write("**Welcome to annual Nerdma Hack-AI-Thon** We need a few details to register you as a hacker.")
-        form1()
-        form2()
-        form3()
-        form4()
-
-        if form5():
-            status_code = submit_application()
-            if status_code == 201:
-                st.session_state.form_submitted = True
-                st.experimental_rerun()
-            else:
-                st.error("Failed to submit the application.")
-
-st.write('</div>', unsafe_allow_html=True)
+# Message to inform applicants that applications are closed
+applications_closed_message = """
+<div class="card">
+    <h2>Thank You for Your Interest</h2>
+    <p>We appreciate your enthusiasm and willingness to participate in our program. However, we regret to inform you that the applications are now closed.</p>
+    <p>We encourage you to stay connected with us for future opportunities. Thank you once again for your interest!</p>
+</div>
+"""
+st.markdown(applications_closed_message, unsafe_allow_html=True)
